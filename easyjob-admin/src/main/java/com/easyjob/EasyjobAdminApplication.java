14c9a8f94d41f0a9e8f93fd54df28c91de2d28b7
GeneClycle <1767386755@qq.com>
2023-10-25 19:51:48 +0800

    springboot项目基本构建

---
+++ easyjob-admin/src/main/java/com/easyjob/EasyjobAdminApplication.java
@@ -0,0 +1,17 @@
+package com.easyjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication(scanBasePackages = {"com.easyjob"})
+@EnableTransactionManagement
+@EnableScheduling
+@EnableAsync
+public class EasyjobAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EasyjobAdminApplication.class,args);
+    }
+}
